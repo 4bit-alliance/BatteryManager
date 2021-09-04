39d68aaaff67a334dfe87ab4a202d90c9e73778c
Kazuki Isogai <i@kazukiisogai.net>
2021-09-04 17:29:47 +0900

    init splash screen frame

--- app/src/main/java/com/fourbitalliance/batterymanager/MainActivity.java
+++ app/src/main/java/com/fourbitalliance/batterymanager/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.Theme_BatteryManager);
         setContentView(R.layout.activity_main);
     }
 }

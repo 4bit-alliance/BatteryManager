dcc879bfb191a723ac1c92db46297ed863d1980b
waigoma <waichi0302@gmail.com>
2021-09-06 21:12:56 +0900

    increase battery info

--- app/src/main/java/com/fourbitalliance/batterymanager/waigoma/BatteryInfo.java
+++ app/src/main/java/com/fourbitalliance/batterymanager/waigoma/BatteryInfo.java
@@ -13,17 +13,34 @@ public class BatteryInfo extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         TextView batteryLevelText = ((AppCompatActivity)context).findViewById(R.id.batteryLevelText);
-        TextView batteryChargingText = ((AppCompatActivity)context).findViewById(R.id.batteryChargingText);
+        TextView batteryStatusText = ((AppCompatActivity)context).findViewById(R.id.batteryStatusText);
         TextView batterySourceText = ((AppCompatActivity)context).findViewById(R.id.batterySourceText);
+        TextView batteryHealthText = ((AppCompatActivity)context).findViewById(R.id.batteryHealthText);
+        TextView batteryTechnologyText = ((AppCompatActivity)context).findViewById(R.id.batteryTechnologyText);
+        TextView batteryTemperatureText = ((AppCompatActivity)context).findViewById(R.id.batteryTemperatureText);
+        TextView batteryVoltageText = ((AppCompatActivity)context).findViewById(R.id.batteryVoltageText);
+        TextView batteryCapacityText = ((AppCompatActivity)context).findViewById(R.id.batteryCapacityText);
+        TextView batteryRemainingText = ((AppCompatActivity)context).findViewById(R.id.batteryRemainingText);
 
-        BatteryInfoAPI bi = new BatteryInfoAPI(intent);
+        BatteryInfoAPI batteryInfo = new BatteryInfoAPI(context, intent);
 
         // バッテリー残量
-        batteryLevelText.setText(bi.level() + "%");
-        // 充電中
-        String charging = bi.isCharging() ? "Charging" : "Discharging";
-        batteryChargingText.setText(charging);
+        batteryLevelText.setText(batteryInfo.level() + "%");
+        // バッテリーステータス
+        batteryStatusText.setText(batteryInfo.status());
         // 給電タイプ
-        batterySourceText.setText(bi.chargeType());
+        batterySourceText.setText(batteryInfo.chargeType());
+        // バッテリー状態
+        batteryHealthText.setText(batteryInfo.health());
+        // バッテリーの種類
+        batteryTechnologyText.setText(batteryInfo.technology());
+        // バッテリー温度
+        batteryTemperatureText.setText(batteryInfo.temperature() + "℃");
+        // バッテリー電圧
+        batteryVoltageText.setText(batteryInfo.voltage() + "V");
+        // バッテリー容量
+        batteryCapacityText.setText(batteryInfo.capacity() + "nAh");
+        // バッテリー残量
+        batteryRemainingText.setText(batteryInfo.remaining() + "nAh");
     }
 }
--- app/src/main/java/com/fourbitalliance/batterymanager/waigoma/BatteryInfoAPI.java
+++ app/src/main/java/com/fourbitalliance/batterymanager/waigoma/BatteryInfoAPI.java
@@ -1,13 +1,16 @@
 package com.fourbitalliance.batterymanager.waigoma;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.BatteryManager;
 
 public class BatteryInfoAPI {
     private final Intent intent;
+    private final BatteryManager batteryManager;
 
-    public BatteryInfoAPI(Intent intent) {
+    public BatteryInfoAPI(Context context, Intent intent) {
         this.intent = intent;
+        batteryManager = (BatteryManager) context.getSystemService(Context.BATTERY_SERVICE);
     }
 
     /**
@@ -19,6 +22,34 @@ public class BatteryInfoAPI {
         int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
         int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
         return (int) (level * 100 / (float) scale);
+    }
+
+    /**
+     * バッテリーステータスを取得する
+     *
+     * @return String <br>
+     *     <ul>
+     *         <li>充電完了</li>
+     *         <li>充電中</li>
+     *         <li>放電中</li>
+     *         <li>充電されていない</li>
+     *         <li>不明</li>
+     *     </ul>
+     */
+    public String status() {
+        int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+        switch (status) {
+            case BatteryManager.BATTERY_STATUS_FULL:
+                return "充電完了";
+            case BatteryManager.BATTERY_STATUS_CHARGING:
+                return "充電中";
+            case BatteryManager.BATTERY_STATUS_DISCHARGING:
+                return "放電中";
+            case BatteryManager.BATTERY_STATUS_NOT_CHARGING:
+                return "充電されていない";
+            default:
+                return "不明";
+        }
     }
 
     /**
@@ -45,9 +76,82 @@ public class BatteryInfoAPI {
         if (!isCharging()) return "Battery";
 
         int chargePlug = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
-        if (chargePlug == BatteryManager.BATTERY_PLUGGED_USB) return "USB";
-        else if (chargePlug == BatteryManager.BATTERY_PLUGGED_AC) return "AC";
+        switch (chargePlug) {
+            case BatteryManager.BATTERY_PLUGGED_USB:
+                return "USB";
+            case BatteryManager.BATTERY_PLUGGED_AC:
+                return "AC";
+            case BatteryManager.BATTERY_PLUGGED_WIRELESS:
+                return "Wireless";
+        }
 
         return "null";
+    }
+
+    /**
+     * バッテリーの健康状態を取得する
+     *
+     * @return String <br>
+     *     <ul>
+     *         <li>良好</li>
+     *         <li>壊れている</li>
+     *         <li>低温状態</li>
+     *         <li>温度異常</li>
+     *         <li>電圧異常</li>
+     *         <li>不明</li>
+     *     </ul>
+     */
+    public String health() {
+        int status = intent.getIntExtra(BatteryManager.EXTRA_HEALTH, -1);
+        switch (status) {
+            case BatteryManager.BATTERY_HEALTH_GOOD:
+                return "良好";
+            case BatteryManager.BATTERY_HEALTH_DEAD:
+                return "壊れている";
+            case BatteryManager.BATTERY_HEALTH_COLD:
+                return "低温状態";
+            case BatteryManager.BATTERY_HEALTH_OVERHEAT:
+                return "温度異常";
+            case BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE:
+                return "電圧異常";
+            default:
+                return "不明";
+        }
+    }
+
+    /**
+     * バッテリーテクノロジーを取得する
+     *
+     * @return String
+     */
+    public String technology() {
+        return intent.getStringExtra(BatteryManager.EXTRA_TECHNOLOGY);
+    }
+
+    /**
+     * バッテリー温度を取得する
+     *
+     * @return int
+     */
+    public float temperature() {
+        return intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, -1) / 10f;
+    }
+
+    /**
+     * バッテリー電圧を取得する (単位: V/ボルト)
+     *
+     * @return float
+     */
+    public float voltage() {
+        int volt = intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, -1);
+        return volt / 1000f;
+    }
+
+    public long capacity() {
+        return batteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_CHARGE_COUNTER);
+    }
+
+    public long remaining() {
+        return batteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_ENERGY_COUNTER);
     }
 }

6a16e8e73d2b15a8ef172512350c1bc7ac5bae12
Kazuki Isogai <i@kazukiisogai.net>
2021-09-10 03:59:26 +0900

    fix main screen design

--- app/src/main/java/com/fourbitalliance/bcon/nagai/Sample.java
+++ app/src/main/java/com/fourbitalliance/bcon/nagai/Sample.java
@@ -39,7 +39,7 @@ public class Sample extends BroadcastReceiver {
 
             // バッテリー残量
             int percentage = -1;
-            if (pm.getBool(PreferenceManager.Settings.ENABLE_LEVEL)) {
+            if (!(pm.getBool(PreferenceManager.Settings.ENABLE_LEVEL))) {
                 percentage = bim.level();
                 // バッテリー情報を取得
                 int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
@@ -67,7 +67,7 @@ public class Sample extends BroadcastReceiver {
             } else {
                 percentage = bim.maxPercentCapacity();
                 long maxPercentCap = bim.maxPercentCapacity();
-                batteryLevel.setText(maxPercentCap + " %");
+                batteryLevel.setText(maxPercentCap + "%");
 
                 if (maxPercentCap >= 80) {
                     batteryState.setText("正常です");
@@ -82,51 +82,100 @@ public class Sample extends BroadcastReceiver {
 
 
             // 画像を表示
-            if (percentage == 100) {
-                batteryImage.setImageResource(R.drawable.bat_100);
-                mlp.setMargins(0, 0,0,0);
+            if (!(pm.getBool(PreferenceManager.Settings.ENABLE_LEVEL))) {
+                if (percentage == 100) {
+                    batteryImage.setImageResource(R.drawable.bat_100);
+                    mlp.setMargins(0, 0,0,0);
 
-            } else if (percentage > 90) {
-                batteryImage.setImageResource(R.drawable.bat_90);
-                mlp.setMargins(0, 70,0,0);
+                } else if (percentage > 90) {
+                    batteryImage.setImageResource(R.drawable.bat_90);
+                    mlp.setMargins(0, 70,0,0);
 
-            } else if (percentage > 80) {
-                batteryImage.setImageResource(R.drawable.bat_80);
-                mlp.setMargins(0,140,0,0);
+                } else if (percentage > 80) {
+                    batteryImage.setImageResource(R.drawable.bat_80);
+                    mlp.setMargins(0,140,0,0);
 
-            } else if (percentage > 70) {
-                batteryImage.setImageResource(R.drawable.bat_70);
-                mlp.setMargins(0, 210, 0, 0);
+                } else if (percentage > 70) {
+                    batteryImage.setImageResource(R.drawable.bat_70);
+                    mlp.setMargins(0, 210, 0, 0);
 
-            } else if (percentage > 60) {
-                batteryImage.setImageResource(R.drawable.bat_60);
-                mlp.setMargins(0,280,0,0);
+                } else if (percentage > 60) {
+                    batteryImage.setImageResource(R.drawable.bat_60);
+                    mlp.setMargins(0,280,0,0);
 
-            } else if (percentage > 50) {
-                batteryImage.setImageResource(R.drawable.bat_50);
-                mlp.setMargins(0,350,0,0);
+                } else if (percentage > 50) {
+                    batteryImage.setImageResource(R.drawable.bat_50);
+                    mlp.setMargins(0,350,0,0);
 
-            } else if (percentage > 40) {
-                batteryImage.setImageResource(R.drawable.bat_40);
-                mlp.setMargins(0, 420, 0, 0);
+                } else if (percentage > 40) {
+                    batteryImage.setImageResource(R.drawable.bat_40);
+                    mlp.setMargins(0, 420, 0, 0);
 
-            } else if (percentage > 30) {
-                batteryImage.setImageResource(R.drawable.bat_30);
-                mlp.setMargins(0, 490, 0, 0);
+                } else if (percentage > 30) {
+                    batteryImage.setImageResource(R.drawable.bat_30);
+                    mlp.setMargins(0, 490, 0, 0);
 
-            } else if (percentage > 20) {
-                batteryImage.setImageResource(R.drawable.bat_20);
-                mlp.setMargins(0, 560, 0, 0);
+                } else if (percentage > 20) {
+                    batteryImage.setImageResource(R.drawable.bat_20);
+                    mlp.setMargins(0, 560, 0, 0);
 
-            } else if (percentage > 10) {
-                batteryImage.setImageResource(R.drawable.bat_10);
-                mlp.setMargins(0,630,0,0);
+                } else if (percentage > 10) {
+                    batteryImage.setImageResource(R.drawable.bat_10);
+                    mlp.setMargins(0,630,0,0);
 
+                } else {
+                    batteryImage.setImageResource(R.drawable.bat_0);
+                    mlp.setMargins(0,700,0,0);
+
+                }
             } else {
-                batteryImage.setImageResource(R.drawable.bat_0);
-                mlp.setMargins(0,700,0,0);
+                if (percentage == 100) {
+                    batteryImage.setImageResource(R.drawable.capa_100);
+                    mlp.setMargins(0, 0,0,0);
 
+                } else if (percentage > 90) {
+                    batteryImage.setImageResource(R.drawable.capa_90);
+                    mlp.setMargins(0, 70,0,0);
+
+                } else if (percentage > 80) {
+                    batteryImage.setImageResource(R.drawable.capa_80);
+                    mlp.setMargins(0,140,0,0);
+
+                } else if (percentage > 70) {
+                    batteryImage.setImageResource(R.drawable.capa_70);
+                    mlp.setMargins(0, 210, 0, 0);
+
+                } else if (percentage > 60) {
+                    batteryImage.setImageResource(R.drawable.capa_60);
+                    mlp.setMargins(0,280,0,0);
+
+                } else if (percentage > 50) {
+                    batteryImage.setImageResource(R.drawable.capa_50);
+                    mlp.setMargins(0,350,0,0);
+
+                } else if (percentage > 40) {
+                    batteryImage.setImageResource(R.drawable.capa_40);
+                    mlp.setMargins(0, 420, 0, 0);
+
+                } else if (percentage > 30) {
+                    batteryImage.setImageResource(R.drawable.capa_30);
+                    mlp.setMargins(0, 490, 0, 0);
+
+                } else if (percentage > 20) {
+                    batteryImage.setImageResource(R.drawable.capa_20);
+                    mlp.setMargins(0, 560, 0, 0);
+
+                } else if (percentage > 10) {
+                    batteryImage.setImageResource(R.drawable.capa_10);
+                    mlp.setMargins(0,630,0,0);
+
+                } else {
+                    batteryImage.setImageResource(R.drawable.capa_0);
+                    mlp.setMargins(0,700,0,0);
+
+                }
             }
+
 
             batteryLevel.setLayoutParams(mlp);
 

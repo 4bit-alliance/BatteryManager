07913458f78af3340bf2ab530d3fc455f195b7c6
waigoma <waichi0302@gmail.com>
2021-09-10 15:26:34 +0900

    move code to createChannel method

--- app/src/main/java/com/fourbitalliance/bcon/MyApplication.java
+++ app/src/main/java/com/fourbitalliance/bcon/MyApplication.java
@@ -5,6 +5,7 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
@@ -12,6 +13,7 @@ import android.os.Build;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import com.fourbitalliance.bcon.nagai.notification.NotificationBattery;
 import com.fourbitalliance.bcon.waigoma.PreferenceManager;
 import com.fourbitalliance.bcon.waigoma.setting.ChangeTheme;
 
@@ -35,29 +37,37 @@ public class MyApplication extends Application {
         else ct.setTheme();
 
         //通知（チャネル作成）
+        NotificationBattery nm = new NotificationBattery();
+        nm.createChannel();
+//        createChannel();
+    }
+
+    public void createChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            String title = this.getString(R.string.app_name);
 
-            NotificationChannel channel = new NotificationChannel("ALARM", "B-CON_ALARMチャネル", importance);
-            NotificationChannel channel2 = new NotificationChannel("WARM", "B-CON_WARMチャネル", importance);
+            NotificationChannel channelBg = new NotificationChannel(Channel.DEFAULT, title, importance);
+            NotificationChannel channel = new NotificationChannel(Channel.ALARM, title + "_ALARMチャネル", importance);
+            NotificationChannel channel2 = new NotificationChannel(Channel.WARN, title + "_WARMチャネル", importance);
 
             channel.setDescription("アラームチャネル");
             channel2.setDescription("警告チャネル");
 
 
             NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channelBg);
             notificationManager.createNotificationChannel(channel);
             notificationManager.createNotificationChannel(channel2);
-
-
         }
     }
 
     public void notifyAlarm() {
         Intent intent = new Intent();
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        PreferenceManager pm = new PreferenceManager(PreferenceManager.Settings.FILE);
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "ALARM")
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, Channel.ALARM)
                 .setSmallIcon(R.drawable.notification_icon)
                 .setContentTitle("警告アラーム")
                 .setContentText("メッセージ：バッテリー残量が100%になりました。")
@@ -77,7 +87,7 @@ public class MyApplication extends Application {
         Intent intent = new Intent();
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "WARM")
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, Channel.WARN)
                 .setSmallIcon(R.drawable.notification_icon)
                 .setContentTitle("充電完了")
                 .setContentText("メッセージ：十分なバッテリー残量になりました。")
@@ -93,7 +103,34 @@ public class MyApplication extends Application {
         notificationManager.notify(R.string.app_name, builder.build());
     }
 
+    public Notification notifyService(Intent intent) {
+        Notification notification = null;
+        int requestCode = intent.getIntExtra("REQUEST_CODE", 0);
+        Context context = getApplicationContext();
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(context, requestCode, new Intent(context.getApplicationContext(), MainActivity.class), PendingIntent.FLAG_IMMUTABLE);
+
+            notification = new Notification.Builder(context, Channel.DEFAULT)
+                    .setSmallIcon(R.drawable.notification_icon)
+                    .setColor(Color.GREEN)
+                    .setContentText("Background稼働中")
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent)
+                    .setWhen(System.currentTimeMillis())
+                    .build();
+        }
+        return notification;
+    }
+
     public static MyApplication getInstance() {
         return instance;
+    }
+
+    public static class Channel {
+        public static String DEFAULT = "default";
+        public static String ALARM = "ALARM";
+        public static String WARN = "WARN";
     }
 }

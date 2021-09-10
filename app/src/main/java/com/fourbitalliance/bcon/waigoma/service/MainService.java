50a1066d6bbf81c9cf398839ee82c078dfb7ea95
waigoma <waichi0302@gmail.com>
2021-09-10 15:43:58 +0900

    implement Service

--- app/src/main/java/com/fourbitalliance/bcon/waigoma/service/MainService.java
+++ app/src/main/java/com/fourbitalliance/bcon/waigoma/service/MainService.java
@@ -1,21 +1,25 @@
 package com.fourbitalliance.bcon.waigoma.service;
 
 import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Build;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 
-import com.fourbitalliance.bcon.R;
+import com.fourbitalliance.bcon.nagai.notification.NotifyManager;
+import com.fourbitalliance.bcon.waigoma.info.BatteryInfoManager;
 
 public class MainService extends Service {
+    private static boolean running = false;
+    private Notification.Builder builder;
+    private final IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -24,47 +28,49 @@ public class MainService extends Service {
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        int requestCode = intent.getIntExtra("REQUEST_CODE",0);
-        Context context = getApplicationContext();
-        String channelId = "default";
-        String title = context.getString(R.string.app_name);
-
-        PendingIntent pendingIntent =
-                PendingIntent.getActivity(context, requestCode, intent, PendingIntent.FLAG_IMMUTABLE);
-
-        NotificationManager notificationManager =
-                (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-        // Notification　Channel 設定
-        NotificationChannel channel = new NotificationChannel(
-                channelId, title , NotificationManager.IMPORTANCE_DEFAULT);
-
-        if(notificationManager != null) {
-            notificationManager.createNotificationChannel(channel);
-
-            Notification notification = new Notification.Builder(context, channelId)
-                    .setSmallIcon(R.drawable.notification_icon)
-                    .setContentText("Background稼働中")
-                    .setAutoCancel(true)
-                    .setContentIntent(pendingIntent)
-                    .setWhen(System.currentTimeMillis())
-                    .build();
-
-            // startForeground
-            startForeground(1, notification);
-        }
+        NotifyManager nm = new NotifyManager();
+        builder = nm.notifyService(intent);
+        Notification notification = builder.build();
+        this.registerReceiver(broadcastReceiver, intentFilter);
+        startForeground(1, notification);
+        running = true;
         return START_REDELIVER_INTENT;
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        unregisterReceiver(broadcastReceiver);
         stopSelf();
+        running = false;
+    }
+
+    private void checkBatteryLevel (Intent intent) {
+        BatteryInfoManager bim = new BatteryInfoManager(intent);
+        NotifyManager nm = new NotifyManager();
+        nm.setNotifyText(builder, bim.level() + " %");
     }
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    // バッテリー残量が変化したら呼ばれる
+    // 今は通知ボックス内のバッテリー残量が変動するだけ
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            checkBatteryLevel(intent);
+        }
+    };
+
+    public static boolean isRunning() {
+        return running;
+    }
+
+    public static void setRunning(boolean b) {
+        running = b;
     }
 }
